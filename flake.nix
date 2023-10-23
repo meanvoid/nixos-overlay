037fe27179ad063ad80d88a1d27e57d7d94f5f0f
Mariia Holovata <ashuramaru@riseup.net>
2023-10-23 15:56:26 +0300

    move thcrap-wrapper to upstream

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "my overlay";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.default
+        ];
+        config = { allowUnfree = true; };
+      };
+    in {
+      overlays.default =  (final: prev: rec {
+        thcrap-wrapper = final.callPackage ./pkgs/misc/thcrap-wrapper {};
+      });
+      packages.x86_64-linux = pkgs;
+    };
+}
