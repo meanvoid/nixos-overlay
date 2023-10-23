ae114f8d794a36eba8155ab05251f6333b237a62
Mariia Holovata <ashuramaru@riseup.net>
2023-10-23 19:40:50 +0300

    testing

--- pkgs/misc/cursors/touhou-cursors/default.nix
+++ pkgs/misc/cursors/touhou-cursors/default.nix
@@ -16,8 +16,8 @@ stdenv.mkDerivation {
     rev = "519e82ca48709cfa71b02bb24c33647307f8eb50";
     sha256 = "0g4w5qb4ggn1q1rn2d2y6blhfnnhlci9d2v9j72123vga0z2hnnn";
   };
-  buildInputs = [bash subversion zenity];
   nativeBuildInputs = [makeWrapper];
+  buildInputs = with pkgs; [zenity];
   installPhase = ''
     mkdir -p $out/bin
     cp thcrap_proton $out/bin/thcrap_proton
