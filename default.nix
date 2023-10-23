1a732ecd8cfaea7cd9b08eb4c886ecd09d7b4aaf
Mariia Holovata <ashuramaru@riseup.net>
2023-10-23 18:47:09 +0300

    testing

--- default.nix
+++ default.nix
@@ -2,5 +2,4 @@
 
 rec {
   thcrap-wrapper = pkgs.callPackage ./pkgs/misc/steam/thcrap-wrapper {};
-  np2kai = pkgs.callPackage ./pkgs/misc/emulators/np2kai {};
 }
--- flake.nix
+++ flake.nix
@@ -10,16 +10,8 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          self.overlays.default
-        ];
-        config = { allowUnfree = true; };
       };
     in {
-      overlays.default =  (final: prev: rec {
-        thcrap-wrapper = final.callPackage ./pkgs/misc/steam/thcrap-wrapper {};
-        # np2kai = final.callPackage ./pkgs/misc/emulators/np2kai {};
-      });
       packages.${system}.default = pkgs.callPackage ./default.nix {};
     };
 }
