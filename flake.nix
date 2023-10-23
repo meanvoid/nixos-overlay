f9524fde408b9eac2b4752b9d8df68964e6e6782
Mariia Holovata <ashuramaru@riseup.net>
2023-10-23 17:07:48 +0300

    move thcrap-wrapper to upstream

--- flake.nix
+++ flake.nix
@@ -10,9 +10,6 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          self.overlays.default
-        ];
         config = { allowUnfree = true; };
       };
     in {
