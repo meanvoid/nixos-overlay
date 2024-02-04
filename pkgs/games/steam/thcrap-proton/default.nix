8c2af9179f443dcf05a318814095be481e1fbd74
2husecondary <2husecondary.stable254@slmails.com>
2024-02-04 18:53:42 +0400

    typo :(
    kept failing to build because of that one bracket

--- pkgs/games/steam/thcrap-proton/default.nix
+++ pkgs/games/steam/thcrap-proton/default.nix
@@ -20,7 +20,7 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "tactikauan";
     repo = "thcrap-steam-proton-wrapper";
-    rev = "47792a3d7fc8c28c409ebcb04d11562979cd6ce0}";
+    rev = "47792a3d7fc8c28c409ebcb04d11562979cd6ce0";
     sha256 = "sha256-qxjHiQrsKQdNQ9eXE7fZ5C738Ae4dG6M1yZc3J38fTU=";
   };
   buildInputs = [
