be7249f1b5e8cd5da4d065cfa63e22cd60134e75
Mariia Holovata <ashuramaru@riseup.net>
2024-02-08 16:26:55 +0200

    downgrade nixpkgs to 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Collection of random packages";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
     pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
     devshell.url = "github:numtide/devshell";
