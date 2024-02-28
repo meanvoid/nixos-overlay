32b3e976b8d84c015c3601bbd72279338308e853
Tenjin <ashuramaru@tenjin-dk.com>
2024-02-28 10:48:13 +0100

    add gradience-git

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -32,6 +32,7 @@
 
       vgpu_unlock = pkgs.callPackage ../modules/vgpu/vgpu_unlock.nix {};
       compile-driver = pkgs.callPackage ../modules/vgpu/compile-driver.nix {};
+      gradience-git = pkgs.callPackage ./misc/gradience-git {};
     };
   };
 }
